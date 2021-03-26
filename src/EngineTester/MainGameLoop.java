8bd6840657e8b0175d53952d8592fda39f0bf17c
VividerAphid <trever.kress@gmail.com>
2021-03-26 16:52:25 -0400

    Added obj file loading

--- src/EngineTester/MainGameLoop.java
+++ src/EngineTester/MainGameLoop.java
@@ -8,6 +8,7 @@ import RenderEngine.DisplayManager;
 import RenderEngine.Loader;
 import Models.RawModel;
 import Models.TexturedModel;
+import RenderEngine.OBJLoader;
 import RenderEngine.Renderer;
 import Shaders.StaticShader;
 import Textures.ModelTexture;
@@ -27,98 +28,100 @@ public class MainGameLoop {
         Loader loader = new Loader();
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
+        
+        RawModel model = OBJLoader.loadObjModel("firstCritter", loader);
 
 
-       float[] vertices = {
-            -0.5f,0.5f,-0.5f,
-            -0.5f,-0.5f,-0.5f,
-            0.5f,-0.5f,-0.5f,
-            0.5f,0.5f,-0.5f,
+//       float[] vertices = {
+//            -0.5f,0.5f,-0.5f,
+//            -0.5f,-0.5f,-0.5f,
+//            0.5f,-0.5f,-0.5f,
+//            0.5f,0.5f,-0.5f,
+//
+//	    -0.5f,0.5f,0.5f,
+//	    -0.5f,-0.5f,0.5f,
+//				0.5f,-0.5f,0.5f,
+//				0.5f,0.5f,0.5f,
+//
+//				0.5f,0.5f,-0.5f,
+//				0.5f,-0.5f,-0.5f,
+//				0.5f,-0.5f,0.5f,
+//				0.5f,0.5f,0.5f,
+//
+//				-0.5f,0.5f,-0.5f,
+//				-0.5f,-0.5f,-0.5f,
+//				-0.5f,-0.5f,0.5f,
+//				-0.5f,0.5f,0.5f,
+//
+//				-0.5f,0.5f,0.5f,
+//				-0.5f,0.5f,-0.5f,
+//				0.5f,0.5f,-0.5f,
+//				0.5f,0.5f,0.5f,
+//
+//				-0.5f,-0.5f,0.5f,
+//				-0.5f,-0.5f,-0.5f,
+//				0.5f,-0.5f,-0.5f,
+//				0.5f,-0.5f,0.5f
+//
+//		};
+//
+//		float[] textureCoords = {
+//
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0,
+//				0,0,
+//				0,1,
+//				1,1,
+//				1,0
+//
+//
+//		};
+//
+//		int[] indices = {
+//				0,1,3,
+//				3,1,2,
+//				4,5,7,
+//				7,5,6,
+//				8,9,11,
+//				11,9,10,
+//				12,13,15,
+//				15,13,14,
+//				16,17,19,
+//				19,17,18,
+//				20,21,23,
+//				23,21,22
+//
+//		};
 
-	    -0.5f,0.5f,0.5f,
-	    -0.5f,-0.5f,0.5f,
-				0.5f,-0.5f,0.5f,
-				0.5f,0.5f,0.5f,
-
-				0.5f,0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f,
-				0.5f,0.5f,0.5f,
-
-				-0.5f,0.5f,-0.5f,
-				-0.5f,-0.5f,-0.5f,
-				-0.5f,-0.5f,0.5f,
-				-0.5f,0.5f,0.5f,
-
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-
-		};
-
-		float[] textureCoords = {
-
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-
-		};
-
-		int[] indices = {
-				0,1,3,
-				3,1,2,
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-
-        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        //RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("huh"));
         TexturedModel staticModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0,0,-1),0,0,0,1);
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
 
         Camera camera = new Camera();
 
         while(!Display.isCloseRequested()){
             //entity.increasePosition(0, 0, 0);
-            entity.increaseRotation(1, 1, 0);
+            entity.increaseRotation(0, 1, 0);
             camera.move();
             renderer.prepare();
             shader.start();
